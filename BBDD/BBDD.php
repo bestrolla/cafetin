815c714bb087840c5ed0b8daf581cb0ea06571ad
bestrolla <angelmanzano01092003@gmail.com>
2026-08-11 12:26:38 -0400

    modificacion 1

--- BBDD/BBDD.php
+++ BBDD/BBDD.php
@@ -33,8 +33,8 @@ class Conexion {
             $this->host = '38.92.48.98';
             $this->port = $port ?: '3306';
             $this->dbname = 'bibliotecamr_cafetin';
-            $this->username = 'bibliotecamr_admin';
-            $this->password = $pass !== false ? $pass : '3qdpqsxlm8y5t0lr';
+            $this->username = 'bibliotecamr_angel';
+            $this->password = $pass !== false ? $pass : 'Isis0109$';
         }
     }
 
